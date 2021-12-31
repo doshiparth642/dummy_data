5669695f82bc27d05399d16d052ddb8ebb284137
unknown <doshiparth642@gmail.com>
2021-12-31 14:53:21 +0530

    first commit

---
+++ Welcome.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
+function Welcome() {
+    let x ='parth'
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default Welcome
